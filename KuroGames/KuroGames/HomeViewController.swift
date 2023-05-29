891e017690149f781cf2620641df8b7374248734
Kel_BLK <keltoum.belkadi@gmail.com>
2023-05-29 15:18:39 +0200

    project

---
+++ KuroGames/KuroGames/HomeViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  KuroGames
+//
+//  Created by Kel_Jellysh on 29/05/2023.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

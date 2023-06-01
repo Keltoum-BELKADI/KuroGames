3c36cbce3946529c12ba6960868661706f6cd86d
Kel_BLK <keltoum.belkadi@gmail.com>
2023-06-01 18:00:03 +0200

    storyboard added elements and launchscreen

---
+++ KuroGames/KuroGames/View/Scene/HomePage/ScanView/ScanViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ScanViewController.swift
+//  KuroGames
+//
+//  Created by Kel_Jellysh on 01/06/2023.
+//
+
+import UIKit
+
+class ScanViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
